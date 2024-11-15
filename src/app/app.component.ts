aea7535681fb4af30686196debff9ff2099cb9df
Yohann ANNETTE <yohann.annette0@gmail.com>
2024-11-15 21:59:44 +0100

    Adding icons and playing card prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from "./components/playing-card/playing-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ PlayingCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
